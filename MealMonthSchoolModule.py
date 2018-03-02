afd26d59f9080817d21c844c36764115b238299b
das-cmu <rishavdas7@gmail.com>
2018-03-01 18:48:07 -0800

    First shot at parsing the data

---
+++ MealMonthSchoolModule.py
@@ -0,0 +1,16 @@
+class MealMonthSchool:
+
+	def __init__(self, county, programGroupYear, fnsReportData, order, programCode, name, cdsCode, siteName, mealTypeCode, mealsServedFree, mealsServedReducedPrice, mealsServedPaid, mealsServedTotal):
+		self.county = county
+		self.programGroupYear = programGroupYear
+		self.fnsReportData = fnsReportData
+		self.order = order
+		self.programCode = programCode
+		self.name = name
+		self.cdsCode = cdsCode
+		self.siteName = siteName
+		self.mealTypeCode = mealTypeCode
+		self.mealsServedFree = mealsServedFree
+		self.mealsServedReducedPrice = mealsServedReducedPrice
+		self.mealsServedPaid = mealsServedPaid
+		self.mealsServedTotal = mealsServedTotal
