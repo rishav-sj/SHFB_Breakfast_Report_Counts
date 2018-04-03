cc6177974a3b341f7ccb689e6461f3e5942d5313
das-cmu <rishavdas7@gmail.com>
2018-04-02 23:30:06 -0700

    Adding more files

---
+++ mealscondenser.py
@@ -0,0 +1,105 @@
+import csv
+
+ignoreList = ['Across the Bridge Foundation', 'ALL SOULS CHURCH SCHOOL', 'Cristo Rey San Jose High School', 'Design Tech High', 'EE S Residential Group Home Inc.', 'Escuela Popular', 'Escuela Xochitl Tonatiul', 'JT Residential Care Facilities', 'Latino College Prep Academy', 'Navigator Schools', 'Sacred Heart Nativity School', 'SAN MATEO CO JUV HALL SCHS', 'SANTA CLARA CO PROBATION', 'South Valley Charter School', 'St Andrew Residential Programs for Youth (STAR HOUSE)', 'ST ELIZABETH SETON SCHOOL', 'ST PATRICK ELEM CATHOLIC', 'Unity Care Group Inc.', 'Voices College-Bound Language', 'REBEKAH CHILDRENS SERVICES']
+
+# Breakfast counts
+schoolDistrictToFreeBreakfastsMap = {}
+schoolDistrictToRPBreakfastsMap = {}
+schoolDistrictToPaidBreakfastsMap = {}
+
+
+# Lunch Counts
+schoolDistrictToFreeLunchesMap = {}
+schoolDistrictToRPLunchesMap = {}
+schoolDistrictToPaidLunchesMap = {}
+
+schoolDistrictToCountyNameMap = {}
+
+
+f = open('meals.csv')
+g = open('mealscondensed.csv', 'w')
+csv_f = csv.reader(f)
+
+def isBreakfast(mealType):
+    return 'BREAKFAST' in mealType;
+
+def isLunch(mealType):
+    return 'LUNCH' in mealType;
+
+def isEligibleCounty(countyName):
+    return (countyName == 'SAN MATEO') or (countyName == 'SANTA CLARA')
+
+def removeCommas(somestring):
+    return somestring.replace(',', '')
+
+def readNumber(entry):
+    if (entry):
+        return long(removeCommas(entry))
+    return 0
+def mapCountyNameToCamelCase(countyName):
+    if (countyName == 'SAN MATEO'):
+      return 'San Mateo'
+    elif (countyName == 'SANTA CLARA'):
+      return 'Santa Clara'
+    else:
+      return countyName
+
+def cleanupSchoolDistrictName(schoolDistrictName):
+    if ('School District' in schoolDistrictName):
+        schoolDistrictName = schoolDistrictName.replace(' School District', '')
+
+    if (schoolDistrictName == 'GILROY UNIFIED SCHOOL DISTRICT'):
+        schoolDistrictName = 'Gilroy Unified'
+    if (schoolDistrictName == 'SANTA CLARA COE'):
+        schoolDistrictName = 'Santa Clara County Office of Education'
+    if (schoolDistrictName == 'MORELAND SCHOOL DISTRICT'):
+        schoolDistrictName = 'Moreland'
+    if (schoolDistrictName == 'LUTHER BURBANK SCHOOL DISTRICT'):
+        schoolDistrictName = 'Luther Burbank'
+    if (schoolDistrictName == 'PACIFICA SCHOOL DISTRICT'):
+        schoolDistrictName = 'Pacifica'
+    return schoolDistrictName
+
+rowNum = 0
+for row in csv_f:
+  if (rowNum != 0):
+    countyName = row[0]
+    schoolDistrictName = removeCommas(row[5])
+    if (isEligibleCounty(countyName) and (not(schoolDistrictName in ignoreList))):
+      mealType = row[8]
+
+      schoolDistrictName = cleanupSchoolDistrictName(schoolDistrictName)
+      if (not(schoolDistrictName in schoolDistrictToFreeBreakfastsMap)):
+        schoolDistrictToFreeBreakfastsMap[schoolDistrictName] = 0
+        schoolDistrictToRPBreakfastsMap[schoolDistrictName] = 0
+        schoolDistrictToPaidBreakfastsMap[schoolDistrictName] = 0
+
+        schoolDistrictToFreeLunchesMap[schoolDistrictName] = 0
+        schoolDistrictToRPLunchesMap[schoolDistrictName] = 0
+        schoolDistrictToPaidLunchesMap[schoolDistrictName] = 0
+
+        schoolDistrictToCountyNameMap[schoolDistrictName] = mapCountyNameToCamelCase(countyName)
+      if (isBreakfast(mealType)):
+        schoolDistrictToFreeBreakfastsMap[schoolDistrictName] = schoolDistrictToFreeBreakfastsMap[schoolDistrictName] + readNumber(row[9])
+        schoolDistrictToRPBreakfastsMap[schoolDistrictName] = schoolDistrictToRPBreakfastsMap[schoolDistrictName] + readNumber(row[10])
+        schoolDistrictToPaidBreakfastsMap[schoolDistrictName] = schoolDistrictToPaidBreakfastsMap[schoolDistrictName] + readNumber(row[11])
+
+      if (isLunch(mealType)):
+        schoolDistrictToFreeLunchesMap[schoolDistrictName] = schoolDistrictToFreeLunchesMap[schoolDistrictName] + readNumber(row[9])
+        schoolDistrictToRPLunchesMap[schoolDistrictName] = schoolDistrictToRPLunchesMap[schoolDistrictName] + readNumber(row[10])
+        schoolDistrictToPaidLunchesMap[schoolDistrictName] = schoolDistrictToPaidLunchesMap[schoolDistrictName] + readNumber(row[11])
+
+  rowNum = rowNum + 1
+
+
+g.write('County, School District, Free Breakfasts, RP Breakfasts, Paid Breakfasts, Free Lunches, RP Lunches, Paid Lunches\n');
+
+for key in schoolDistrictToFreeBreakfastsMap:
+    strToWrite = schoolDistrictToCountyNameMap[key] + "," + key + "," + str(schoolDistrictToFreeBreakfastsMap[key]) + "," + str(schoolDistrictToRPBreakfastsMap[key]) + "," + str(schoolDistrictToPaidBreakfastsMap[key]) + "," + str(schoolDistrictToFreeLunchesMap[key]) + "," + str(schoolDistrictToRPLunchesMap[key]) + "," + str(schoolDistrictToPaidLunchesMap[key]) + '\n' 
+    g.write(strToWrite)
+
+
+
+
+f.close()
+g.close()
---
+++ script.sh
@@ -0,0 +1,3 @@
+python frpmcondenser.py
+python mealscondenser.py
+python frpmmealscondenser.py
